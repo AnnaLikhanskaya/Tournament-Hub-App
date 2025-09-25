d75b5aca549261d22ea9237b67ea02e4313758f9
AnnaLikhanskaya <a.lihansky@yandex.ru>
2025-09-25 10:56:32 +0300

    Система управления школьными спортивными турнирами

---
+++ src/main/java/ru/school/tournamenthub/TournamentHubApplication.java
@@ -0,0 +1,9 @@
+package ru.school.tournamenthub;
+
+import org.springframework.boot.SpringApplication;
+
+public class TournamentHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentHubApplication.class, args);
+    }
+}
